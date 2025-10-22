6386ffdfaf997238ca840e6da4ce37a97389e047
veltroptj06-cell <veltroptj06@gmail.com>
2025-10-22 21:12:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,18 @@
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import scipy.odr as odr
 
 import os
 
 import numpy as np
-import pandas as pd
-from pandas import DataFrame, Series  # for convenience
 
 import pims
 import trackpy as tp
 ##---------##
-PixelsPerParticle = 53
-minmas = 100
-path = ""# path to folder with imigaes
+PixelsPerParticle = 25
+minmas = 1200
+path = "C:/Users/trist/OneDrive/Documents/DinoCapture 3.0/poging2"# path to folder with imigaes
 ##---------##
 
 
@@ -22,24 +21,57 @@ path = ""# path to folder with imigaes
 def gray(image):
     return image[:, :, 1]  # Take just the green channel
 
+direct = os.listdir(path)
 
-files = os.listdir(path)
-frames = []
-for file in files:
-    frames.append(gray(pims.open(path + "/" + file)))
+class dataPoint:
+    def __init__(self, avarage, std, name):
+        self.avarage = avarage
+        self.std = std
+        self.name = name
+datapoints = []
 
+for directory in range(15):
+    npath = path + "/" + str(directory+1)
+    files = os.listdir(npath)
+    frames = []
+    for file in files:
+        frames.append(gray(pims.open(npath + "/" + file)))
 
+    pointsArr = np.zeros(len(frames))
 
-for i in range(len(frames)):
-    f = tp.locate(frames[i], PixelsPerParticle, invert=True, minmass=minmas)
-    print(len(f["mass"]))
-    tp.annotate(f, frames[i][0])
+    for i in range(len(frames)):
+        f = tp.locate(frames[i], PixelsPerParticle, invert=True, minmass=minmas)
+        pointsArr[i] = len(f["mass"])
+        print(len(f["mass"]))
+        tp.annotate(f, frames[i][0])
+        
+ #       fig, ax = plt.subplots()
+ #       ax.hist(f['mass'], bins=20)
     
-    fig, ax = plt.subplots()
-    ax.hist(f['mass'], bins=20)
-#plt.show()
-# Optionally, label the axes.
-    ax.set(xlabel='mass', ylabel='count')
+  #      ax.set(xlabel='mass', ylabel='count')
     
-    tp.subpx_bias(f)
-    plt.show()
+  #      tp.subpx_bias(f)
+   #     plt.show()
+
+    datapoints.append(dataPoint(pointsArr.mean(), pointsArr.std(), directory))
+
+    print("Total detected points: ", pointsArr.sum())
+    print("Samples : ", len(frames))
+    print("Avarage points : ", pointsArr.mean()-6, " +-", pointsArr.std())
+#%%
+xassp = [0,5,10,15,20,30,40,55,75,100,130,165,215,300,385]
+
+xass=[]
+yass=[]
+yerror = []
+xerror = []
+for i in datapoints:
+    xass.append(xassp[int(i.name)]*8e-7)
+    yass.append(i.avarage)
+    yerror.append(i.std)
+    xerror.append(0.5*8e-7)
+
+print(xass)
+print(yass)
+print(yerror)
+print(xerror)
